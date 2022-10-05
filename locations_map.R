6544db27848c4b46afcfb5d9158d78141e06d726
Tomasz Zielinski <tomasz.zielinski@ed.ac.uk>
2022-10-06 00:34:39 +0100

    clock clubbers locations map

---
+++ locations_map.R
@@ -0,0 +1,25 @@
+
+library(htmlwidgets)
+library(leaflet)
+library(dplyr)
+
+data = read.table("./UK_Clock_Club-geolocations.csv", header =TRUE, sep = ",") 
+
+data = select(data, Location, Long, Lat, Number_of_subscriptions)
+data = rename(data, Value = Number_of_subscriptions)
+
+dotsColor =  colorBin("plasma", domain = 0:70, bins = 7)
+
+
+loc_map <- leaflet(data) %>% 
+  addTiles() %>% 
+  setView(-3.475300, 55.89687, zoom = 7) %>%
+  addCircles(lng = data$Long,lat = data$Lat, label = data$Location,
+             weight =20, color = ~dotsColor(data$Value), radius = data$Value*100, opacity = 0.7) %>%
+  addLegend(data= data,"bottomright",pal = dotsColor, values = data$Value,
+                       title = "Subscriptions", opacity = 1)  
+  
+  
+  
+saveWidget(widget = loc_map, file = "clock-clubbers.html", ) 
+
